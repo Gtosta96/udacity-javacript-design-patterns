db44179d6f09588c45aa6f53c1d6d042f3caa6b7
Gtosta96 <gabrieltosta3@gmail.com>
2017-12-07 00:30:28 -0300

    added 2 cat-clicker

--- cat-clicker/index.js
+++ cat-clicker/index.js
@@ -1,10 +1,19 @@
 window.onload = () => {
-  const catImg = document.getElementById('catImg');
-  const catCounter = document.getElementById('catCounter');
+  const catImgOne = document.getElementById('catImgOne');
+  const catCounterOne = document.getElementById('catCounterOne');
 
-  catCounter.textContent = 0;
+  const catImgTwo = document.getElementById('catImgTwo');
+  const catCounterTwo = document.getElementById('catCounterTwo');
 
-  catImg.addEventListener('click', () => {
-    catCounter.textContent = parseInt(catCounter.textContent) + 1;
-  }, false);
+  catCounterOne.textContent = 0;
+  catCounterTwo.textContent = 0;
+
+  const updateCatClickerCounter = (el) => {
+    return () => {
+      el.textContent = parseInt(el.textContent) + 1;
+    }
+  }
+
+  catImgOne.addEventListener('click', updateCatClickerCounter(catCounterOne), false);
+  catImgTwo.addEventListener('click', updateCatClickerCounter(catCounterTwo), false);
 }
