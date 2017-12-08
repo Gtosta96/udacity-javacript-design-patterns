d67e1a90325179610b7d6ebb3b6814e2c9e5f0db
Gtosta96 <gabrieltosta3@gmail.com>
2017-12-08 17:09:49 -0200

    addes some style / made cat's name logic

--- cat-clicker/index.js
+++ cat-clicker/index.js
@@ -1,18 +1,19 @@
 window.onload = () => {
+  const catNameOne = document.getElementById('catNameOne');
   const catImgOne = document.getElementById('catImgOne');
   const catCounterOne = document.getElementById('catCounterOne');
 
+  const catNameTwo = document.getElementById('catNameTwo');
   const catImgTwo = document.getElementById('catImgTwo');
   const catCounterTwo = document.getElementById('catCounterTwo');
+
+  catNameOne.textContent = 'Horacio';
+  catNameTwo.textContent = 'Jubileu';
 
   catCounterOne.textContent = 0;
   catCounterTwo.textContent = 0;
 
-  const updateCatClickerCounter = (el) => {
-    return () => {
-      el.textContent = parseInt(el.textContent) + 1;
-    }
-  }
+  const updateCatClickerCounter = (el) => () => el.textContent = parseInt(el.textContent) + 1;
 
   catImgOne.addEventListener('click', updateCatClickerCounter(catCounterOne), false);
   catImgTwo.addEventListener('click', updateCatClickerCounter(catCounterTwo), false);
